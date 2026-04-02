af3de6162d781ef42898b0ae319f0f9762abcd08
Yayier Quispe <yayier.dev@gmail.com>
2026-04-02 12:53:12 -0500

    Proyecto CRUD

---
+++ app_crud/Controllers/ProductoController.cs
@@ -0,0 +1,86 @@
+﻿using app_crud.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlClient.DataClassification;
+using System.Data;
+
+namespace app_crud.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly IConfiguration _config;
+        public ProductoController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        IEnumerable<ProductoModel> productos()
+        {
+            List<ProductoModel> listaProductos = new List<ProductoModel>();
+
+            using (SqlConnection cn = new SqlConnection(_config["ConnectionStrings:conexSQL"]))
+            {
+                cn.Open();
+
+                SqlCommand cmd = new SqlCommand("exec usp_productos", cn);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    listaProductos.Add(new ProductoModel
+                    {
+                        idProducto = dr.GetInt32(0),
+                        descripcion = dr.GetString(1),
+                        uMedida = dr.GetString(2),
+                        precio = dr.GetDecimal(3),
+                        stock = dr.GetInt32(4),
+                    });
+                }
+                dr.Close();
+            }
+            return listaProductos;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductoModel obj)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(obj);
+            }
+            
+            string mensaje = "";
+            using (SqlConnection cn = new SqlConnection(_config["ConnectionStrings:conexSQL"]))
+            {
+                await cn.OpenAsync();
+
+               
+                SqlCommand cmd = new SqlCommand("usp_insertar_producto", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@descripcion", obj.descripcion);
+                cmd.Parameters.AddWithValue("@uMedida", obj.uMedida);
+                cmd.Parameters.AddWithValue("@precio", obj.precio);
+                cmd.Parameters.AddWithValue("@stock", obj.stock);
+
+                int filas = await cmd.ExecuteNonQueryAsync();
+
+                mensaje = $"Se inserto {filas} registros";
+            }
+            TempData["mensaje"] = mensaje;
+            return RedirectToAction("index");
+        }
+
+
+        public async Task<ActionResult> Index()
+        {
+            return View(await Task.Run(() => productos()));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ app_crud/Models/ProductoModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace app_crud.Models
+{
+    public class ProductoModel
+    {
+        [Display(Name = "Id Producto")]
+        public int idProducto { get; set; }
+        [Display(Name = "Descripcion")]
+        public string descripcion { get; set; }
+        [Display(Name = "Unid Medida")]
+        public string uMedida { get; set; }
+        [Display(Name = "Precio Unitario")]
+        public decimal precio { get; set; }
+        [Display(Name = "Stock")]
+        public int stock { get; set; }
+
+    }
+
+
+}
